106f4ed8f4a76205b017afbda535811b9c58f007
AbdellahBellakrim <bellakrim2032@gmail.com>
2024-05-22 21:59:59 +0100

    chore: Update build configuration, dependencies, and code structure New: Home page, Login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import NotFound from "./routes/NotFoundPage";
 import HomePage from "./routes/HomePage";
+import Login from "./routes/Login";
 
 function App() {
   return (
-    <div className="w-screen h-screen overflow-x-hidden overflow-y-auto App">
+    <div className="w-screen h-screen overflow-x-hidden overflow-y-auto App bg-[#FAFAFA] text-black light">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
--- client/src/routes/HomePage.tsx
+++ client/src/routes/HomePage.tsx
@@ -1,9 +1,38 @@
+import { Button } from "@nextui-org/react";
+import { useNavigate } from "react-router-dom";
+
 function HomePage() {
+  const navigate = useNavigate();
   return (
-    <div className="w-full h-full bg-white flex justify-center items-center text-black font-semibold text-2xl underline">
-      HomePage
+    <div className="w-full h-full flex justify-center items-center flex-col gap-10 md:max-w-[920px] mx-auto p-4 relative">
+      <div className="flex justify-center items-center gap-1 w-full">
+        <div className="w-fit h-fit bg-white bg-opacity-5">
+          <img
+            className="bg-white bg-opacity-5"
+            src="logo-devlinks-small.svg"
+            alt="logo-devlinks-small.svg"
+            loading="lazy"
+            width={45}
+            height={45}
+          />
+        </div>
+        <h1 className="text-4xl font-semibold text-[#333333]">devLinks</h1>
+      </div>
+      <div className="w-full flex justify-center items-center gap-7 overflow-hidden">
+        <Button
+          className="dark bg-[#633CFF] rounded-md font-medium"
+          onClick={() => navigate("/login")}
+        >
+          Login
+        </Button>
+        <Button className="dark bg-[#633CFF] rounded-md font-medium">
+          Sign Up
+        </Button>
+      </div>
     </div>
   );
 }
 
 export default HomePage;
+
+// w-full max-w-[396px] p-0 h-12 rounded-md font-semibold
